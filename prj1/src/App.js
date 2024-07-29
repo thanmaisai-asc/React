f65ab1fbcc92ed8a950676c41684e3386fa29ce9
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-07-29 12:28:05 +0530

    first project - props

---
+++ prj1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Thanmai Sai" />
+    </div>
+  );
+}
+
+export default App;
---
+++ prj1/src/components/greet.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Greet(props) {
+    return (
+        <h1>Welcome to react {props.name}</h1>
+    );
+}
