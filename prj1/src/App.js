46a3a4d681097acbacd1aba8feeb5d7f13348d25
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-07 10:53:07 +0530

    react - router - dom

--- prj1/src/App.js
+++ prj1/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import HookCounter from "./hooks/HookCounter";
-// import ClassCounter from "./hooks/ClassCounter";
-// import HookCountertwo from './hooks/HookCoutertwo';
-import UseStateObject from './hooks/UseStateObject';
+import HookArray from "./hooks/Arrayhook";
 
 //install axios
 function App() {
   return (
     <div>
-     <UseStateObject/>
+      <HookArray/>
     </div>
   );
 }
