b50568747d1c618c2ca8d5c11325670d9c5b84cc
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-07 16:24:15 +0530

    adding filters for search bar func

--- prj2/src/components/Users.js
+++ prj2/src/components/Users.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useSearchParams } from 'react-router-dom'
 
 export default function Users() {
+  const [searchParams,setSearchParams] = useSearchParams();
   return (
     <div>
         <h2>User 1</h2>
         <h2>User 2</h2>
         <h2>User 3</h2>
         <Outlet/>
+        <button onClick={()=>{setSearchParams({filter:'active'})}}>Active</button>
+        <button onClick={()=>{setSearchParams({})}}>Reset</button>
     </div>
   )
 }
