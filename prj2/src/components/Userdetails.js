91dd21338f0eea9b6c2dfe616b35b0afc683fb86
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-07 16:14:51 +0530

    passing dynamic params

--- prj2/src/components/Userdetails.js
+++ prj2/src/components/Userdetails.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function UserDetails() {
+    const urlParams = useParams()
+    const uid = urlParams.userId
   return (
-    <div>UserDetails</div>
+    <div>UserDetails  {uid}</div>
   )
 }
